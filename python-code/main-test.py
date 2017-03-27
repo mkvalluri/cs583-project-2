9d4b12dc8c2cd1400f47ec9cc5d76fdcbb73230a
Murali Krishna Valluri <Murali Krishna Valluri>
2017-03-27 16:07:29 -0500

    Added TfidfTransformer

--- python-code/main-test.py
+++ python-code/main-test.py
@@ -1,24 +1,34 @@
-data = ['You won something!', 'How are you', 'Murali won ssdf']
-is_spam = [1, 0, 1]
+data = ['You won something!', 'are you asdasd', 'How are you', 'Murali won ssdf','how won asdsdf', 'Krishna won how is ur health', 'Your how name']
+is_spam = [1, 1, 0, 1, 1, 0, 0]
 
 from sklearn.feature_extraction.text import CountVectorizer
 vect = CountVectorizer()
 vect.fit(data)
 print vect.get_feature_names()
 
-train_dtm = vect.transform(data)
+train_dtm_counts = vect.transform(data)
+print train_dtm_counts.shape
+
+from sklearn.feature_extraction.text import TfidfTransformer
+tfidf_transformer = TfidfTransformer()
+train_dtm = tfidf_transformer.fit_transform(train_dtm_counts)
 print train_dtm.shape
 
 from sklearn.naive_bayes import MultinomialNB
 clf = MultinomialNB().fit(train_dtm, is_spam)
 
 data_new = ['how won asdsdf', 'Krishna won how is ur health', 'Your how name']
-test_dtm = vect.transform(data_new)
+#test_dtm = vect.transform(data_new)
 actual = [1, 0, 0]
-predicted = clf.predict(test_dtm)
-target_names = ['No', 'Yes']
-for d, c in zip(data_new, predicted):
-    print ('%r => %s' % (d, target_names[c]))
+#predicted = clf.predict(test_dtm)
+#target_names = ['No', 'Yes']
+#for d, c in zip(data_new, predicted):
+#    print ('%r => %s' % (d, target_names[c]))
 
-from sklearn import metrics
-print(metrics.classification_report(actual, predicted, target_names=target_names))
+from sklearn.model_selection import cross_val_score
+scores = cross_val_score(clf, train_dtm, is_spam, cv=3)
+print scores
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+#from sklearn import metrics
+#print(metrics.classification_report(actual, predicted, target_names=target_names))
